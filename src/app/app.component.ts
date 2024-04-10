fb99e5863f71fb5923ff8c6d900b2ea1dc59b32e
Lei <lei.dev.Entwicklung@gmail.com>
2024-04-09 22:04:36 -0400

    added a category form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MatButtonModule, MatSlideToggleModule,
+    HomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-sales-app';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DashboardComponent },
+    // { path: 'home', component: HomeComponent },
+    { path: 'categories', component: CategoriesComponent },
+    // {path: '/suppliers', component: }
+    { path: '', redirectTo: 'home', pathMatch: "full" }
+];
